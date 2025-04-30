8d00cd829d053835bdbb4a72dd39484a6158d30b
Edeline24 <145514721+Edeline24@users.noreply.github.com>
2025-04-30 11:49:49 +0000

    update the xampp to edel database

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import mysql.connector
 try:
     db = mysql.connector.connect(
         host="127.0.0.1",  # localhost if MySQL is on the same machine
-        port= 3306,  # Use the correct port (default is 3306)
+        port= 3307,  # Use the correct port (default is 3306)
         user="root",  # root user, change if needed
         password="",  # Your password for root (empty if none)
-        database="SavvyLingua"  # Make sure this database exists
+        database="savvylinguadb"  # Make sure this database exists
     )
 
     cursor = db.cursor()
